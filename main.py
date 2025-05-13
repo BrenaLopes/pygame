303a8736f0acbb2ec399ded7a459d74de102f1c7
BrenaLopes <brenalf@al.insper.edu.br>
2025-05-13 14:08:21 +0000

    jogo_inspraçao

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
- import pygame
-from player import Player
-from sprites import SpriteAnimado
-from constantes_jogo import *
-from fases import Fases
-from ui import UI
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.janela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA), pygame.FULLSCREEN)
-        pygame.display.set_caption("Meu Jogo")
-        self.clock = pygame.time.Clock()
+# Importando Bibliotecas
+import pygame
+import time
+from assets import load_assets, SANTALIGHT, SANTABLACK, HOHOHO_SOUND, JUMP_SOUND, EAT_SOUND, DEATH_SOUND
+from config import WIDTH, HEIGHT, FPS, WALKING, INIT, GAME, DEAD, QUIT, CHOOSE
+from classes import Background, Santa, Snowball, Cookie
+from init_screen import init_screen
+from choose_screen import choose_screen
+from game_screen import game_screen
+from death_screen import death_screen
 
-        # Criar a instância do personagem (Player)
-        self.player = Player((100, 100), [pygame.sprite.Group()], "frames_player", "sprite_colisao", "frames_escada")
-        
-        # Outras inicializações necessárias, como fases e UI
-        self.fases = Fases()
+# Inicialização padrão
+pygame.init()
+pygame.mixer.init()
 
-    def run(self):
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            
-            # Atualiza o jogo
-            self.fases.update()
-            self.player.update()
 
-            # Desenha tudo na tela
-            self.janela.fill((255, 255, 255))  # Limpa a tela
-            self.player.draw(self.janela)
+# ----- Gera tela principal
+janela = pygame.display.set_mode([WIDTH,HEIGHT]) # define uma surface ("janela" que o jogo será exibido)
+pygame.display.set_caption("Late Santa") # define um nome para a janela aberta
 
-            pygame.display.update()
-            self.clock.tick(60)
+#Definindo recorde como 0
+record = 0
 
-        pygame.quit()
+#Estado inicial: INIT
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = init_screen(janela, record)
 
-if __name__ == "__main__":
-    game = Game()
-    game.run()
+    elif state == CHOOSE:
+        #Recebe o estado do jogo e o sprite escolhido pelo jogador
+        returns = choose_screen(janela)
+        state = returns[0]
+        sprite_jogo = returns[1]
+
+    elif state == GAME:
+        #Recebe o estado do jogo, o score e o record
+        returns = game_screen(janela, record, sprite_jogo)
+        state = returns[0]
+        score = returns[1]
+        record = returns[2]
+
+    elif state == DEAD:
+        state = death_screen(janela, score)
+
+    else:
+        state = QUIT
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
