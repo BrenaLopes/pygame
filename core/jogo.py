e70dfc6fa22767089b04b421cecf42a49e23bf61
dwongz6 <davidhwmw@al.insper.edu.br>
2025-05-26 17:23:48 -0300

    sons

--- core/jogo.py
+++ core/jogo.py
@@ -1,15 +1,13 @@
 import pygame
 import time
 import random
-import os
 from config import *
 from core.jogador import Jogador
 from core.vilao import Vilao, VilaoForte
 from core.projeto import Projeto, TiroVilao
-from core.imagens import carregar_personagem, carregar_tiro
+from core.imagens import carregar_personagem, carregar_tiro, carregar_cenario
 from core.moeda import Moeda
-from core.imagens import carregar_cenario, carregar_personagem, carregar_tiro
-
+from core.sons import tocar_musica_de_fundo, tocar_som_tiro  # <-- IMPORTAMOS O NOVO SOM
 
 def desenhar_barra_vida(tela, vida):
     largura_barra = 200
@@ -20,8 +18,10 @@ def desenhar_barra_vida(tela, vida):
 
 def executar_jogo(nome_jogador):
     pygame.init()
+    tocar_musica_de_fundo()  # Som de fundo
+
     tela = pygame.display.set_mode((LARGURA, ALTURA))
-    pygame.display.set_caption("Harry Potter e as Relíquias da morte")
+    pygame.display.set_caption("Harry Potter e as Relíquias da Morte")
     clock = pygame.time.Clock()
 
     # Cenários
@@ -34,8 +34,7 @@ def executar_jogo(nome_jogador):
     img_tiro = carregar_tiro()
     img_vilao = carregar_personagem("vilao.png", tamanho=(80, 80))
     img_moeda = carregar_personagem("moeda.png", tamanho=(40, 40))
-    img_vilao_forte = carregar_personagem("vilao_forte.gif", tamanho = (100,100))
-
+    img_vilao_forte = carregar_personagem("vilao_forte.gif", tamanho=(100, 100))
 
     fonte = pygame.font.SysFont("Arial", 24)
     fonte_aviso = pygame.font.SysFont("Arial", 28, bold=True)
@@ -58,15 +57,14 @@ def executar_jogo(nome_jogador):
     tempo_para_viloes_atirarem = 50
     tempo_para_viloes_fortes = tempo_para_viloes_atirarem + 20
     invulneravel_ate = start_time + 10
-
     transicao_duracao = 3
+
     rodando = True
 
     while rodando:
         clock.tick(FPS)
         tempo_jogo = time.time() - start_time
 
-        # Eventos
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 rodando = False
@@ -74,11 +72,11 @@ def executar_jogo(nome_jogador):
                 if moedas_coletadas >= 5:
                     novo_tiro = Projeto(jogador.x + jogador.largura, jogador.y + jogador.altura // 2)
                     feitiços.append(novo_tiro)
+                    tocar_som_tiro()  # <-- SOM AO ATIRAR
 
         teclas = pygame.key.get_pressed()
         jogador.mover(teclas)
 
-        # Moedas
         if tempo_jogo >= tempo_para_moedas and random.random() < 0.02:
             moedas.append(Moeda(img_moeda))
 
@@ -93,20 +91,16 @@ def executar_jogo(nome_jogador):
                     jogador.vida = min(100, jogador.vida + 20)
                     moedas_para_curar += cura_a_cada
 
-        # Vilões comuns
         if len(viloes) < 5:
             novo = Vilao(img_vilao)
             novo.vel_x = max(-12, -3 - pontuacao // 50)
             viloes.append(novo)
 
-        # Vilões fortes
         if tempo_jogo >= tempo_para_viloes_fortes and len(viloes_fortes) < 2:
             viloes_fortes.append(VilaoForte(img_vilao_forte))
 
-        # Movimentação e colisões
         for vilao in viloes + viloes_fortes:
             vilao.mover()
-
             if tempo_jogo >= tempo_para_viloes_atirarem and random.random() < 0.01:
                 tiros_vilao.append(TiroVilao(vilao.rect.x, vilao.rect.centery))
 
@@ -138,20 +132,18 @@ def executar_jogo(nome_jogador):
                         if vilao in viloes:
                             viloes.remove(vilao)
                             viloes.append(Vilao(img_vilao))
-                            pontuacao+=10
+                            pontuacao += 10
                         elif vilao in viloes_fortes:
                             viloes_fortes.remove(vilao)
                             viloes_fortes.append(VilaoForte(img_vilao_forte))
-                        pontuacao += 25
+                            pontuacao += 25
                     break
 
-        # Atualiza feitiços
         for f in feitiços[:]:
             f.mover()
             if f.rect.x > LARGURA:
                 feitiços.remove(f)
 
-        # Atualiza tiros dos vilões
         for t in tiros_vilao[:]:
             t.mover()
             if t.rect.colliderect(jogador.rect):
@@ -163,7 +155,6 @@ def executar_jogo(nome_jogador):
             elif t.rect.right < 0:
                 tiros_vilao.remove(t)
 
-        # CENÁRIO com transições suaves
         if tempo_jogo < tempo_para_viloes_atirarem:
             fundo_atual = cenario_dia
             proximo_fundo = cenario_tarde
@@ -175,7 +166,6 @@ def executar_jogo(nome_jogador):
         else:
             fundo_atual = cenario_noite
             proximo_fundo = None
-            tempo_alvo = None
 
         if proximo_fundo:
             progresso = max(0, min(1, (tempo_jogo - (tempo_alvo - transicao_duracao)) / transicao_duracao))
@@ -187,16 +177,18 @@ def executar_jogo(nome_jogador):
         else:
             tela.blit(fundo_atual, (0, 0))
 
-        # Desenhar elementos
         jogador.desenhar(tela)
-        for v in viloes + viloes_fortes: v.desenhar(tela)
-        for f in feitiços: f.desenhar(tela, img_tiro)
-        for t in tiros_vilao: pygame.draw.rect(tela, AMARELO, t.rect)
-        for moeda in moedas: moeda.desenhar(tela)
+        for v in viloes + viloes_fortes:
+            v.desenhar(tela)
+        for f in feitiços:
+            f.desenhar(tela, img_tiro)
+        for t in tiros_vilao:
+            pygame.draw.rect(tela, AMARELO, t.rect)
+        for moeda in moedas:
+            moeda.desenhar(tela)
 
         desenhar_barra_vida(tela, jogador.vida)
         tela.blit(fonte.render(f"Pontos: {pontuacao}", True, BRANCO), (10, 10))
-
         texto_moedas = fonte.render(f'Pomos de ouro: {moedas_coletadas}', True, AMARELO)
         tela.blit(texto_moedas, (LARGURA - texto_moedas.get_width() - 10, 10))
 
@@ -204,12 +196,9 @@ def executar_jogo(nome_jogador):
             aviso = fonte_aviso.render(f'Pegue {5 - moedas_coletadas} pomos de ouro para atirar!', True, DOURADO)
             tela.blit(aviso, ((LARGURA - aviso.get_width()) // 2, ALTURA - 40))
 
-        
         pygame.display.flip()
 
     from core.ranking import salvar_pontuacao, exibir_ranking
     salvar_pontuacao(nome_jogador, pontuacao)
     pygame.quit()
     exibir_ranking()
-
-    
---
+++ core/sons.py
@@ -0,0 +1,12 @@
+import pygame
+
+def tocar_musica_de_fundo():
+    pygame.mixer.init()
+    pygame.mixer.music.load("sons/Música HP fundo.mp3")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(-1)
+
+def tocar_som_tiro():
+    efeito = pygame.mixer.Sound("sons/Tiro do Hp.mp3")
+    efeito.set_volume(0.6)
+    efeito.play()
