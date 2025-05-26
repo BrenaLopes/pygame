e69838d8d2e7a4351bc4da39738cb16e59f982e8
BrenaLopes <brenalf@al.insper.edu.br>
2025-05-26 19:56:24 -0300

    ajuste volume

--- core/sons.py
+++ core/sons.py
@@ -2,21 +2,21 @@ import pygame
 
 def tocar_musica_de_fundo():
     pygame.mixer.init()
-    pygame.mixer.music.load("sons/Música HP fundo.mp3")
-    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.load("pygame\sons\Música HP fundo.mp3")
+    pygame.mixer.music.set_volume(0.6)
     pygame.mixer.music.play(-1)
 
 def tocar_som_tiro():
-    efeito = pygame.mixer.Sound("sons/Tiro do Hp.mp3")
-    efeito.set_volume(0.6)
+    efeito = pygame.mixer.Sound("pygame\sons\Tiro do Hp.mp3")
+    efeito.set_volume(0.2)
     efeito.play()
 
 def tocar_som_dano():
-    efeito = pygame.mixer.Sound("sons/Dano sofrido.mp3")
-    efeito.set_volume(0.6)
+    efeito = pygame.mixer.Sound("pygame\sons\Dano sofrido.mp3")
+    efeito.set_volume(0.3)
     efeito.play()
 
 def tocar_som_moeda():
-    efeito = pygame.mixer.Sound("sons/Pegando pomo.mp3")
-    efeito.set_volume(0.6)
+    efeito = pygame.mixer.Sound("pygame\sons\Pegando pomo.mp3")
+    efeito.set_volume(0.2)
     efeito.play()
